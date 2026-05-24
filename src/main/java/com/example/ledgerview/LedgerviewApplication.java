00f38d1548d5bfddb6e56da6d0e19de3f9416be6
mikolajdevs <mikolajdevs@gmail.com>
2026-05-24 15:24:18 +0200

    chore: init spring backend

---
+++ src/main/java/com/example/ledgerview/LedgerviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.ledgerview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerviewApplication.class, args);
+	}
+
+}
