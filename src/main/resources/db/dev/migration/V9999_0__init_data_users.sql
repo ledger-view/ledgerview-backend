8b40c0a199d7f3f8c2296ebb30beafcec2fc46d2
mikolajdevs <mikolajdevs@gmail.com>
2026-05-26 16:36:05 +0200

    feature: add db seed migrations

---
+++ src/main/resources/db/dev/migration/V9999_0__init_data_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO ledgerview.users (id, email, external_id)
+VALUES ('00000000-0000-0000-0000-000000000001', 'admin@example.com', 'keycloak-external-id-admin'),
+       ('00000000-0000-0000-0000-000000000002', 'user@example.com', 'keycloak-external-id-user'),
+       ('00000000-0000-0000-0000-000000000003', 'guest@example.com', 'keycloak-external-id-guest');
